179f311b3fac7d1c0ba9ff854730eb1c8393538d
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-11-07 20:28:40 -0700

    rendering in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
 
   return (
-    <div className="App">
-      <div>
-        <h1>Test title</h1>
-        <ol className="test-list">
-          <li>List item 1</li>
-          <li>List item 2</li>
-          <li>List item 3</li>
-        </ol>
-        <svg>
-          <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
-        </svg>
-        <form>
-          <input type="text" />
-        </form>
-      </div>
+    <div>
+      <h1>Animals: </h1>
+      <ul>
+        {animalsList}
+      </ul>
     </div>
   );
 }
